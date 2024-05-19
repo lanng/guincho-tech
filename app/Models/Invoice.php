570adde0b9e29fa6ff000c96b7329d7cf448f5c9
Victor Languer <victorlanguer@hotmail.com>
2024-05-19 18:35:48 -0300

    feat: Add initial models and migrations

---
+++ app/Models/Invoice.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Invoice extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'insurance_id',
+        'company_id',
+        'invoice_number',
+        'value',
+        'issue_date',
+        'due_date',
+        'status',
+    ];
+}
