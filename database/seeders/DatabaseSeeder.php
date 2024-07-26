d272fbe7c25f6d59adeade7b07bfd5afc4beedba
Victor Languer <victorlanguer@hotmail.com>
2024-07-26 12:47:45 -0300

    feat: making seeder to a normal and admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\UserRoleEnum;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,6 +19,12 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'role' => UserRoleEnum::OFFICE,
+        ]);
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@test.com',
+            'role' => UserRoleEnum::ADMIN,
         ]);
     }
 }
